c89a885a11aa5004cf6eaaa2368e7ae559184760
ZetaPlusPlus <berkut.47su.ta@gmail.com>
2020-08-16 17:07:42 +0900

    no message

--- InteractiveSimulation/Output/pyConsole00.py
+++ InteractiveSimulation/Output/pyConsole00.py
@@ -3,40 +3,50 @@
 description:
     
 --------------------------------------------------------------------------------"""
+'''----------------------------------------------------------------------
+load modules
+----------------------------------------------------------------------'''
 import csv
 import os
 import time
-#import sys
-#import pygame
 import tkinter as tk
 import tkinter.ttk as ttk
 
 
-"""----------------------------------------------------------------------
-main script (define global objects)
-----------------------------------------------------------------------"""
+
+'''----------------------------------------------------------------------
+main script (define script-global objects)
+----------------------------------------------------------------------'''
+'''--------------------
+setting about time handling
+--------------------'''
 timeBegin=time.time()
 timeLim=10000
 tInterval=10
 
-rootframe= tk.Tk()
 
+'''--------------------
+setting about gui
+--------------------'''
+rootframe= tk.Tk()  # main gui window
+
+# table display gui
 treeview=ttk.Treeview(rootframe)
 treeview["show"]= "headings"
 treeview["columns"]=(1,2)
-
 treeview.heading(1, text="variable")
 treeview.heading(2, text="value")
 treeview.pack()
 
 
 
-"""----------------------------------------------------------------------
+'''----------------------------------------------------------------------
 define sub-routines
-----------------------------------------------------------------------"""
+----------------------------------------------------------------------'''
 def readcsv():
-    curDir=os.getcwd()
+    curDir=os.getcwd()  # get directory of this script
     
+    # open and read simulation data csv
     with open(curDir+'\pyConsole00_data.csv') as dataFile:
         reader = csv.reader(dataFile)
         i=0
@@ -57,13 +67,17 @@ def readcsv():
 def mainroutine():
     global rootframe
     
+    # read data csv
     [dataMatrix, nRow, nCol]= readcsv()
+    
     x = treeview.get_children()
     
+    # delete table displayed on window
     for item in x:
         treeview.delete(item)
     #----- end for -----
     
+    # re-display info of scv data
     timeRunning= time.time() - timeBegin
     treeview.insert("","end",values=("time (in python script)", timeRunning))
     
@@ -72,16 +86,19 @@ def mainroutine():
         treeview.insert("","end",values=(dataMatrix[i][0],dataMatrix[i][1]))
     #***** end for *****
     
+    # command of recursive call, with specific time interval
     rootframe.after(tInterval, mainroutine)
 #***** end def *****
 
 
 
-"""----------------------------------------------------------------------
+'''----------------------------------------------------------------------
 main script
-----------------------------------------------------------------------"""
+----------------------------------------------------------------------'''
+# read data csv
 [dataMatrix, nRow, nCol]= readcsv()
 
+# display info of scv data
 timeRunning= time.time() - timeBegin
 treeview.insert("","end",values=("time(in python script)", timeRunning))
 i=0
@@ -90,10 +107,9 @@ for i in range(nRow):
 #***** end for *****
 
 
+# call routine of display & update, with specific time interval
 rootframe.after(tInterval, mainroutine)
+
+# continue display & update of csv data until window is closed
 rootframe.mainloop()
-
-    
-
-
 
